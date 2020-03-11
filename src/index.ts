669dec105b4bf83ebf66f3bff8f1d365f8bc36ce
Miguel Panuto <miguel.panuto@live.com>
2020-03-11 15:42:12 -0300

    Firs Commit backend

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
+
+mongoose.connect('mongodb+srv://simple_user:Y1Tj3r8dZzaeRzRN@devcluster-y40dm.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    });
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333);
