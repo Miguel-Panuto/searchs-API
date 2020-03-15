69213c7ba4236d9f9bb81b094fae07c8da02a5c3
Miguel Panuto <miguel.panuto@live.com>
2020-03-15 14:20:38 -0300

    Scripting to make public

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-#Searchs Project REST API
+# Searchs Project REST API
+This is academic, I'm madding 6 types of searchs, 3 without infos and 3 with info, this just the backend server, the front and mobile will be done in a near future.
+
+## Commands
+dev -> "npm run dev" or "yarn dev"
+
+### Necessary configurations
+I created a folder called config on the project root, and a file inside, called "dev.env" and put the MongoDB address URL, in there I have 2 variables "DB_URL", this is where I will put the mongo address and the PORT. 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
-mongoose.connect('mongodb://simple_user:pbZ98wsoOWEUWgGi@devcluster-shard-00-00-y40dm.mongodb.net:27017,devcluster-shard-00-01-y40dm.mongodb.net:27017,devcluster-shard-00-02-y40dm.mongodb.net:27017/test?ssl=true&replicaSet=DevCluster-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -12,4 +12,4 @@ const app = express();
 app.use(express.json()); // Define that uses json before defining rotues!!!!!
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT);
